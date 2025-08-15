8ce5c4f023057f3f78681da867a111b8aeb8c190
Brad9232 <brandonj.rebello@gmail.com>
2025-08-16 00:00:06 +1000

    Add files via upload

---
+++ server.js
@@ -0,0 +1,25 @@
+
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const connectDB = require('./config/db');
+const itemsRouter = require('./routes/itemRoutes');
+
+dotenv.config();
+
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use('/api/auth', require('./routes/authRoutes'));
+//app.use('/api/tasks', require('./routes/taskRoutes'));
+
+if (require.main === module) {
+    connectDB();
+    const PORT = process.env.PORT || 5001;
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  }
+
+
+module.exports = app
